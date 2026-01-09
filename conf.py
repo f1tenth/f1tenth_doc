8cf556fe0e19d207a61ce11612057bea5eae82b1
Ahmad Amine <ahmadamine998@gmail.com>
2026-01-09 15:51:51 -0500

    Add uv integration

--- conf.py
+++ conf.py
@@ -77,8 +77,6 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     # 'display_version': True,  # Display the docs version
     # 'navigation_depth': 4,  # Depth of the headers shown in the navigation bar
-
-    "flyout_display": "attached",
 }
 
 # VCS options: https://docs.readthedocs.io/en/latest/vcs.html#github
